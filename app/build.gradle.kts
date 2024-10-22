fce56a4e500c751d9dee1ba5d51ec4f8df0548e3
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2024-10-22 21:21:01 +0700

    add layout library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.TugasAkhir.qolami"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.ssp.android)
+    implementation(libs.sdp.android)
 }
